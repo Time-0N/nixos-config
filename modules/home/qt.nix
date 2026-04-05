f0af8b3247a409c506bad0c405e697e6dcb4b9a8
Time-0N <timeon.haas@gmail.com>
2026-04-05 02:30:09 +0200

    Added qt styling

--- modules/home/qt.nix
+++ modules/home/qt.nix
@@ -5,12 +5,28 @@
     qt6Packages.qt6ct
     libsForQt5.qtstyleplugin-kvantum
     qt6Packages.qtstyleplugin-kvantum
-    orchis-theme
+    (catppuccin-kvantum.override {
+      accent = "blue";
+      variant = "mocha";
+    })
   ];
 
   qt = {
     enable = true;
     platformTheme.name = lib.mkForce "qtct";
     style.name = lib.mkForce "kvantum";
+  };
+
+  xdg.configFile = {
+    "Kvantum/catppuccin-mocha-blue".source = "${
+      pkgs.catppuccin-kvantum.override {
+        accent = "blue";
+        variant = "mocha";
+      }
+    }/share/Kvantum/catppuccin-mocha-blue";
+    "Kvantum/kvantum.kvconfig".text = ''
+      [General]
+      theme=catppuccin-mocha-blue
+    '';
   };
 }
