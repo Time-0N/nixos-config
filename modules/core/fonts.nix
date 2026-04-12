5b59a5ea1ef44ac637954d677626af705e7c5e79
Time-0N <timeon.haas@gmail.com>
2026-04-12 22:30:33 +0200

    feat(font) add new fonts
    Signed-off-by: Time-0N <timeon.haas@gmail.com>

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.code-new-roman
-    nerd-fonts.geist-mono
-  ];
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.code-new-roman
+      nerd-fonts.geist-mono
+      open-sans
+      overpass
+    ];
+    fontconfig.enable = true;
+  };
 }
