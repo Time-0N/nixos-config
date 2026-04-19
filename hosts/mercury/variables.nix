eee3d6de095cac34dddba4a579acc66ff96ce7ad
Time-0N <timeon.haas@gmail.com>
2026-04-20 00:23:50 +0200

    feat(vars) add mimeDefaultApps
    Signed-off-by: Time-0N <timeon.haas@gmail.com>

--- hosts/mercury/variables.nix
+++ hosts/mercury/variables.nix
@@ -14,7 +14,49 @@
   tailscale = false;
 
   mimeDefaultApps = {
-    "text/plain" = [ "neovim-kitty.desktop" ];
+    # File Manager
     "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+
+    # Text Editor
+    "text/plain" = [ "neovim-kitty.desktop" ];
+    "text/markdown" = [ "neovim-kitty.desktop" ];
+    "text/x-python" = [ "neovim-kitty.desktop" ];
+    "text/x-shellscript" = [ "neovim-kitty.desktop" ];
+    "text/x-c" = [ "neovim-kitty.desktop" ];
+    "text/x-rust" = [ "neovim-kitty.desktop" ];
+    "application/json" = [ "neovim-kitty.desktop" ];
+    "text/xml" = [ "neovim-kitty.desktop" ];
+
+    # Web Browser
+    "text/html" = [ "zen.desktop" ];
+    "x-scheme-handler/http" = [ "zen.desktop" ];
+    "x-scheme-handler/https" = [ "zen.desktop" ];
+    "x-scheme-handler/about" = [ "zen.desktop" ];
+    "x-scheme-handler/unknown" = [ "zen.desktop" ];
+
+    # Images
+    "image/jpeg" = [ "imv.desktop" ];
+    "image/png" = [ "imv.desktop" ];
+    "image/gif" = [ "imv.desktop" ];
+    "image/webp" = [ "imv.desktop" ];
+    "image/svg+xml" = [ "imv.desktop" ];
+
+    # Audio & Video
+    "video/mp4" = [ "vlc.desktop" ];
+    "video/x-matroska" = [ "vlc.desktop" ];
+    "video/webm" = [ "vlc.desktop" ];
+    "video/x-msvideo" = [ "vlc.desktop" ];
+    "audio/mpeg" = [ "vlc.desktop" ];
+    "audio/flac" = [ "vlc.desktop" ];
+    "audio/ogg" = [ "vlc.desktop" ];
+    "audio/x-wav" = [ "vlc.desktop" ];
+
+    # Documents
+    "application/pdf" = [ "org.gnome.Evince.desktop" ];
+    "application/postscript" = [ "org.gnome.Evince.desktop" ];
+
+    # Torrents
+    "application/x-bittorrent" = [ "org.qbittorrent.qBittorrent.desktop" ];
+    "x-scheme-handler/magnet" = [ "org.qbittorrent.qBittorrent.desktop" ];
   };
 }
