a1317bf3f6982234dd01a6b0d139265a9e468c87
Time-0N <timeon.haas@gmail.com>
2026-05-01 02:04:18 +0200

    refactor(boot) switch to tuigreet
    Signed-off-by: Time-0N <timeon.haas@gmail.com>

--- modules/core/displaymanager.nix
+++ modules/core/displaymanager.nix
@@ -5,39 +5,27 @@
   ...
 }:
 {
-  programs.regreet = {
-    enable = true;
-
-    settings = {
-      background = {
-        path = "/etc/regreet/wallpaper.jpg";
-        fit = "Cover";
-      };
-      GTK = lib.mkForce {
-        icon_theme_name = "Adwaita";
-        theme_name = "Adwaita-dark";
-        cursor_theme_name = "Adwaita";
-      };
-      appearance = {
-        greeting = "Wake up, ${vars.username}...";
-      };
-    };
-  };
-
   services.greetd = {
     enable = true;
     settings = {
       default_session = {
-        command = "${lib.getExe pkgs.cage} -s -- ${lib.getExe pkgs.regreet}";
+        command = ''
+          ${lib.getExe pkgs.tuigreet} \
+            --time \
+            --remember \
+            --remember-user-session \
+            --asterisks \
+            --greeting "Arise my creation, ${vars.hostName}..." \
+            --theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red' \
+            --cmd "uwsm start hyprland-uwsm.desktop"
+        '';
         user = "greeter";
       };
     };
   };
 
-  users.extraUsers.greeter = {
-    extraGroups = [
-      "video"
-      "input"
-    ];
-  };
+  # Needs to be writable by tuigreet stores last session
+  systemd.tmpfiles.rules = [
+    "d /var/cache/tuigreet 0755 greeter greeter - -"
+  ];
 }
