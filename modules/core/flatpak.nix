490baf24fb13779f9dbdfccffc9433f2295109e5
Time-0N <timeon.haas@gmail.com>
2026-05-08 20:40:11 +0200

    feat(fatpak) add bambu studio
    Signed-off-by: Time-0N <timeon.haas@gmail.com>

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -8,6 +8,7 @@
         "com.github.tchx84.Flatseal" # Manage flatpak permissions - should always have this
         "io.github.flattool.Warehouse" # Manage flatpaks, clean data, remove flatpaks and deps
         "hu.irl.cameractrls" # Camera management
+        "com.bambulab.BambuStudio" # Bambu Studio
       ];
 
       update.onActivation = true;
