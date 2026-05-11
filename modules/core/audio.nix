6ce5b89694978dc0562ffaf8835a771886d03f03
Time-0N <timeon.haas@gmail.com>
2026-05-11 17:56:00 +0200

    feat(pipewire) expanded sample rates
    Signed-off-by: Time-0N <timeon.haas@gmail.com>

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -6,6 +6,20 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
+
+    extraConfig.pipewire."10-rates" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.allowed-rates" = [
+          44100
+          48000
+          88200
+          96000
+          176400
+          192000
+        ];
+      };
+    };
   };
 
   security.rtkit.enable = true;
